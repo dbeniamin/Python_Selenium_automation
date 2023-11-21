aaa89422b59b0d9cc7fc02f37df632f93e10dcd8
Drimus Beniamin <104926843+dbeniamin@users.noreply.github.com>
2023-11-21 15:21:15 +0200

    Add files via upload

---
+++ file_download.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time 
+
+driver = webdriver.Chrome()
+driver.get('https://the-internet.herokuapp.com/download')
+driver.maximize_window()
+# use to wait for the full page to load - set max time to wait as argument for the implicitly wait
+driver.implicitly_wait(10)
+# use xpath to find element
+# use value attribute from xpath to target the position of file to be downloaded ie: [the postion of the file ]
+# only usable if the downloadable items are all in the same class
+# for other cases you might need to locate each element separatelly
+driver.find_element (By.XPATH, '//*[@id="content"]/div/a[19]').click()
+# use time sleep to see the file downloaded before closing the page
+# the site does not have a confirmation message for download so you need to wait
+time.sleep(5)
+
+driver.quit
