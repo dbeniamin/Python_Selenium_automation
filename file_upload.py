74675d3967d8337c72fd1d7ba2c51dabcb2e4145
Drimus Beniamin <104926843+dbeniamin@users.noreply.github.com>
2023-11-21 15:22:27 +0200

    file_upload.py

--- file_upload.py
+++ file_upload.py
@@ -8,21 +8,21 @@ driver.maximize_window()
 driver.implicitly_wait(10)
 
 upload_file = driver.find_element (By.CSS_SELECTOR, '#file-upload')
-# for some reasons you cant upload files that are on the root user location - no idea why 
-# the path from windows will be "C:\folder name\ etc" you need to add "\" after C: for the code to work because path is not absolute C:
-# the file name and location has to be specific for your machine
+  # for some reasons you cant upload files that are on the root user location - no idea why 
+  # the path from windows will be "C:\folder name\ etc" you need to add "\" after C: for the code to work because path is not absolute C:
+  # the file name and location has to be specific for your machine
 upload_file.send_keys("C:\\your folder name\another folder name\file name.png")
 driver.implicitly_wait(10)
-# add a sleep sequence to see the file sent to the upload field
+  # add a sleep sequence to see the file sent to the upload field
 time.sleep(3)
-# find the submit button and pass the .click() action
+  # find the submit button and pass the .click() action
 driver.find_element(By.CSS_SELECTOR, "#file-submit").click()
 driver.implicitly_wait(10)
-# creeate a result element so you can pass an assertion
-# can be passed without creating the variable but i prefer to use it like this for visibility
+  # creeate a result element so you can pass an assertion
+  # can be passed without creating the variable but i prefer to use it like this for visibility
 result = driver.find_element(By.CSS_SELECTOR, "#uploaded-files")
-# assertion to check if the succesfull upload action has the selected file name
-# test using the .text method to match the strings
+  # assertion to check if the succesfull upload action has the selected file name
+  # test using the .text method to match the strings
 assert result.text == "file name.png"
 time.sleep(2)
 driver.quit
